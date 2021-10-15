90b95f24629d1df3ce980d585d39af3f57fe37de
saiqulkencoz <mbekqwerty@gmail.com>
2021-10-15 08:00:38 +0700

    toast message

--- app/src/main/java/com/example/profileview/MainActivity.java
+++ app/src/main/java/com/example/profileview/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.profileview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void toastMsg(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void displayToastMsg(View v) {
+        toastMsg("wa.me/+6287794163130");
     }
 }
